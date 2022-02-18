d19699fed0df3d0633cdd60f65dde6bc7b521b22
truongbk48 <50911151+DoBaTruong@users.noreply.github.com>
2022-02-18 20:29:24 +0700

    Add files via upload

--- src/js/pages/Details.js
+++ src/js/pages/Details.js
@@ -1,16 +1,19 @@
 import axios from 'axios'
 import { Fragment, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import HomeSlider from '../components/HomeSlider'
 import { GET_NEWS_URI } from '../config/api'
 
-const getNews = (slug, callback) => {
+const getNews = (slug, callback, callbackTwo) => {
     axios({
         method: 'get',
         url: GET_NEWS_URI + `/${slug}`
     })
     .then(res => {
+        console.log(res.data)
         if(res.data.code === 200 || !res.data.code) {
             callback(res.data.news)
+            callbackTwo(res.data.relateds)
         }
     })
 }
@@ -18,8 +21,10 @@ const getNews = (slug, callback) => {
 const Details = ({...props}) => {
     const {slug} = useParams()
     const [news, setNews] = useState(null)
+    const [relateds, setRelateds] = useState([])
+
     useEffect(() => {
-        getNews(slug, setNews)
+        getNews(slug, setNews, setRelateds)
     }, [])
 
     useEffect(() => {
@@ -39,6 +44,9 @@ const Details = ({...props}) => {
                         <p className='page__news--time'>{(new Date(news.published_at)).toLocaleDateString('en-US', {day: 'string', year: 'numeric', month: 'long', day: 'numeric'})}<span></span></p>
                         <div id='content'></div>
                         <p className='page__news--author'>{news.authors}</p>
+                        <section className='news__slider'>
+                            <HomeSlider banners={relateds} />
+                        </section>
                     </Fragment>
                 )}
             </section>
